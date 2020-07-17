ef8b5cba87ba4c7b04d6b6dd21e4ef32d6b9cf58
Paramjeet Singh <45735028+Paramjeetsingh9582@users.noreply.github.com>
2020-07-17 23:53:46 +0530

    second commit

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,24 +1,36 @@
-<?php
+<?php 
+$errors = '';
+$myemail = 'singhparamjeet9250@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
-    $name = $_POST['name']; 
-    $visitor_email = $_POST['email'];
-    $message = $_POST['comment'];
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
 
-    $email_from = "singhparamjeet9582@gmail.com";
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
 
-    $email_subject = "New Form submission";
-
-    $email_body = "User Name: $name.\n".
-                      "User Email: $visitor_email.\n".
-                          "User Message: $message.\n"; 
-
-    $to = "singhparamjeet9250@gmail.com";
-
-    $headers = "Form: $email_from \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to, $email_subject, $email_body, $headers);
-
-    header("Location: index.html");
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: index.html');
+} 
 ?>
